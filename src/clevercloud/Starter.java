4c85f03b576dcbdb934a0a2d5ebe40c9fd4d1fa1
Philippe Charrière <ph.charriere@gmail.com>
2017-09-29 16:31:36 +0200

    2nd

--- src/clevercloud/Starter.java
+++ src/clevercloud/Starter.java
@@ -3,27 +3,11 @@ package clevercloud;
 import java.lang.String;
 import java.lang.System;
 import java.io.IOException;
-import java.io.*;
 
 public class Starter {
 
   public static void main(String [] args) throws IOException, InterruptedException {
-    System.out.println("Hello 🌍");
-    /*
-    String cmd = System.getenv("SHELL_SCRIPT").toString();
-    
-    Runtime run = Runtime.getRuntime();
-    Process pr = run.exec(cmd);
-    pr.waitFor();
-    BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-    String line = "";
-    while ((line=buf.readLine())!=null) {
-      System.out.println(line);
-    }
-    */
+    System.out.println("👋 🌍 welcome on Clever Cloud");
   }
 
 }
-/*
-SHELL_SCRIPT=/app/storage/xl_deploy/bin/run.sh
-*/
