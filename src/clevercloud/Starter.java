e8254a36e470869c7c43018131bae3b848885111
Philippe Charrière <ph.charriere@gmail.com>
2017-09-29 17:46:33 +0200

    3thd

--- src/clevercloud/Starter.java
+++ src/clevercloud/Starter.java
@@ -3,11 +3,22 @@ package clevercloud;
 import java.lang.String;
 import java.lang.System;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Starter {
 
   public static void main(String [] args) throws IOException, InterruptedException {
     System.out.println("👋 🌍 welcome on Clever Cloud");
+    Timer timer = new Timer();
+    TimerTask myTask = new TimerTask() {
+      @Override
+      public void run() {
+        System.out.println("❤️ ... ❤️ ... I'm alive 😀");
+      }
+    };
+
+    timer.schedule(myTask, 2000, 10000);
   }
 
 }
